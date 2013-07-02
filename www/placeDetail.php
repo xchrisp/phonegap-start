fec581e59e102231923b7351cd4c454bb9816819
xchrisp <5366260064@fitm.kmutnb.ac.th>
2013-07-02 23:57:31 +0700

    Update placeDetail.php

--- www/placeDetail.php
+++ www/placeDetail.php
@@ -6,7 +6,7 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 <table width="300" border="0" cellspacing="2" cellpadding="0">
   <tr>
     <td width="10" rowspan="4">
-    <img src="<a href="http://image.ohozaa.com/view2/wVURlF1JvD7sQi3k" target="_blank"><img border="0" src="http://image.ohozaa.com/i/6f0/jRdOrP.jpg"
+    <img src="http://image.ohozaa.com/view2/wVURlF1JvD7sQi3k" target="_blank"><img border="0" src="http://image.ohozaa.com/i/6f0/jRdOrP.jpg"
      width="130" height="80">
     </td>
     <td width="10">&nbsp;</td>
@@ -15,9 +15,5 @@ header("Cache-Control: post-check=0, pre-check=0", false);
   <tr>
     <td>&nbsp;</td>
     <td>ที่ตั้ง : 129 ต.เนนหอม อ.เมือง จ.ปราจีนบุรี </td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>รายละเอียด</td>
   </tr>
 </table>
