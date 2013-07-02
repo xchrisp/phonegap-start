e0e8c9bd0bf125e9a93959942ada2405de276b5a
xchrisp <5366260064@fitm.kmutnb.ac.th>
2013-07-02 23:50:42 +0700

    placeDetail.php
    ไว้สำหรับเรียกดู Detail Marker

---
+++ www/placeDetail.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-type:text/html; charset=UTF-8");        
+header("Cache-Control: no-store, no-cache, must-revalidate");       
+header("Cache-Control: post-check=0, pre-check=0", false);       
+?>
+<table width="300" border="0" cellspacing="2" cellpadding="0">
+  <tr>
+    <td width="10" rowspan="4">
+    <img src="<a href="http://image.ohozaa.com/view2/wVURlF1JvD7sQi3k" target="_blank"><img border="0" src="http://image.ohozaa.com/i/6f0/jRdOrP.jpg"
+     width="130" height="80">
+    </td>
+    <td width="10">&nbsp;</td>
+    <td width="264">ชื่อสถานที่ : มหาวิทยาลัยพระจอมเกล้าพระนครเหนือ วิทยาเขตปราจีนบุรี</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>ที่ตั้ง : 129 ต.เนนหอม อ.เมือง จ.ปราจีนบุรี </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>รายละเอียด</td>
+  </tr>
+</table>
